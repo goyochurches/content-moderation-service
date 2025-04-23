3cde63dc26824f9c0070cc293dfb1d47545fa00f
goyochurches <goyochurches@gmail.com>
2025-04-23 22:55:26 +0200

    feat/ update changes

--- src/main/java/com/user/flag/config/AppConfig.java
+++ src/main/java/com/user/flag/config/AppConfig.java
@@ -1,12 +1,10 @@
 package com.user.flag.config;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@EnableFeignClients(basePackages = "com.user.flag.client")
 public class AppConfig {
 
     @Bean
